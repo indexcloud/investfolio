8e69ce25adbe05585ba6a166f49742add32dbdde
Jason Deng <foralief@gmail.com>
2021-01-25 23:34:07 -0600

    Adding chartjs

---
+++ src/containers/Portfolios.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Chart from "../components/Chart";
+
+class Portfolios extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Welcome to Portfolios</h1>
+				<Chart />
+			</div>
+		);
+	}
+}
+
+export default Portfolios;
