5a25697ebd662145dd507ec21a334d7e3a3220d8
Jason Deng <foralief@gmail.com>
2021-01-30 09:22:42 -0600

    Fixing anchor link bug

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -6,9 +6,7 @@ class Homepage extends Component {
 			<div>
 				<h1>Welcome to Investfolio</h1>
 				<h5>
-					<a href="https://www.reddit.com/r/wallstreetbets/" target="_blank">
-						I want to Yolo!
-					</a>
+					<a href="https://www.reddit.com/r/wallstreetbets/">I want to Yolo!</a>
 				</h5>
 			</div>
 		);
